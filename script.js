33a0b5d97933d9591c7c0018bab87e6ba612f43e
Šimon Bernard <113086082+SimonBer7@users.noreply.github.com>
2023-02-08 21:47:09 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const home = document.getElementById("home");
+const contact = document.getElementById("contact");
+const about = document.getElementById("about");
+
+let text = document.getElementById("text");
+
+home.addEventListener("click", e => {
+
+    text.style.fontSize = 50;
+    text.style.color = "blue";
+    
+
+
+
+});
+
+
+
+
